c264bc74ab865817938333ad66eb4eb90fb2445e
Rafael Santana <rafaelsantanaep@gmail.com>
2023-10-06 12:00:10 -0300

    Why this change was necessary? We were using the .to_json() function from pandas, which for some dataframes generate an inconsistent view of the data, which makes the data arrives at snowflake in a single variant column because snowpark is unable to infer the schema of the table.
    How does it address the problem?
    - By using to_parquet() instead of to_json.
    - We had some conversion issues using the default engine (pyarrow), so, I decided to used the 'fastparquet' engine instead.
    
    Related tickets or issues: [SOLENG-5620]

--- steps/save_data_in_snowflake/save_data_in_snowflake.py
+++ steps/save_data_in_snowflake/save_data_in_snowflake.py
@@ -1,54 +1,45 @@
-from snowflake.snowpark import Session
 from dadosfera.services.snowflake import get_snowpark_session
-import sys
-import json
+from snowflake.snowpark import functions as F
+from typing import List, Dict
 import logging
+import json
+import sys
 import os
-import pandas as pd
-from typing import Dict, Union, List
-from functools import reduce
-
-ORCHEST_STEP_UUID = os.environ.get('ORCHEST_STEP_UUID')
-
 
 logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def save_objects_in_snowflake_using_copy(
+ORCHEST_STEP_UUID = os.environ.get('ORCHEST_STEP_UUID')
+
+def save_data_in_snowflake(
     secret_id: str,
-    objects: List[Dict[str, Union[str, bytes]]],
-    table_identifier: str
+    table_identifier: str,
+    snowflake_queries_object: Dict
 ) -> None:
 
-    try:
-        session = get_snowpark_session(secret_id)
-        session.use_schema('PUBLIC')
-        dfs = [pd.DataFrame(_object['file_content']) for _object in objects]
-        result_pdf = reduce(lambda x, y: pd.concat([x, y], axis=0), dfs)
-        result_pdf.to_parquet('temp', engine='fastparquet')
+    if len(snowflake_queries_object['queries']) > 1:
+        raise Exception('The number of queries is greater than one')
 
-        transformed_tbl_identifier = table_identifier.replace('"', '').replace('.', '_')
-        create_temp_stage = "create or replace temporary stage my_internal_stage"
-        session.sql(create_temp_stage).collect()
+    if len(snowflake_queries_object['post_actions']) > 0:
+        raise Exception('The number of post_actions is greater than 0')
 
-        put_command = f"PUT file://temp @my_internal_stage/{transformed_tbl_identifier}"
-        session.sql(put_command).collect()
-        result_df = session.read.parquet(f"@my_internal_stage/{transformed_tbl_identifier}")
-        result_df.write.mode('overwrite').save_as_table(table_identifier)
-    finally:
-        os.remove('temp')
+    session = get_snowpark_session(secret_id)
+    df = session.sql(snowflake_queries_object['queries'][0])
+    df.write.mode('overwrite').save_as_table(table_identifier)
+
 
 def orchest_handler():
     import orchest
+    secret_id = orchest.get_step_param('secret_id')
     table_identifier = orchest.get_step_param('table_identifier')
     incoming_variable_name = orchest.get_step_param('incoming_variable_name')
-    secret_id = orchest.get_step_param('secret_id')
-    objects = orchest.get_inputs()[incoming_variable_name]
-    save_objects_in_snowflake_using_copy(
-        objects=objects,
+    snowflake_queries_object = orchest.get_inputs()[incoming_variable_name]
+    save_data_in_snowflake(
+        secret_id=secret_id,
         table_identifier=table_identifier,
-        secret_id=secret_id
+        snowflake_queries_object=snowflake_queries_object
     )
 
 def script_handler():
@@ -57,17 +48,16 @@ def script_handler():
     config_json = sys.argv[1]
     config = json.loads(config_json)
 
+    secret_id = config["secret_id"]
     input_filepath = config['input_filepath']
     table_identifier = config['table_identifier']
-    secret_id = config['secret_id']
 
     with open(input_filepath,'r') as f:
-        objects = json.load(f)
-
-    save_objects_in_snowflake_using_copy(
-        objects=objects,
+        snowflake_queries_object = json.load(f)
+    save_data_in_snowflake(
+        secret_id=secret_id,
         table_identifier=table_identifier,
-        secret_id=secret_id
+        snowflake_queries_object=snowflake_queries_object
     )
 
 if __name__ == "__main__":
