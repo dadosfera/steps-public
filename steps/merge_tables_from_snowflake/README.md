2d2a138bfddecb3da4d437ef033a156211265d66
paulomcmiller <paulomcmiller@gmail.com>
2024-04-30 14:23:14 -0300

    UPDATE: merge step refactor, Add README

---
+++ steps/merge_tables_from_snowflake/README.md
@@ -0,0 +1,55 @@
+```
+# Step de Mesclar Tabelas no Snowflake
+
+Este STEP permite mesclar tabelas alvo no Snowflake usando dados de entrada fornecidos de algum processo anterior ou mesmo de outro ativo de dados no Snowflake. O STEP possui uma feature que procura compatibilizar os DataTypes entre as colunas das tabelas Source e Target.
+
+## Recursos
+
+- Mescla tabelas do Snowflake com base em colunas especificadas.
+- Suporta execução como parte de uma orquestração ou como um script autônomo.
+- Flexibilidade na configuração das tabelas de origem e de destino, bem como das colunas usadas para a fusão.
+
+## Requisitos
+
+- Python 3.x
+- Biblioteca do Snowflake
+- Biblioteca Pandas
+- Biblioteca Snowpark
+
+## Uso
+
+### Execução como parte de uma Orquestração
+
+Quando executado como parte de uma orquestração, este STEP espera os seguintes parâmetros:
+
+- `secret_id`: As credenciais do cliente no Snowflake.
+- `source_table`: O nome da tabela de origem no Snowflake ou os dados de saída de um step anterior em formato DataFrame do Pandas, em lista de dicionários ou arquivos .csv, .json e .parquet.
+- `target_table`: O nome da tabela de destino no Snowflake.
+- `source_on`: As colunas na tabela de origem para a junção.
+- `target_on`: As colunas na tabela de destino para a junção.
+
+### Execução como um Script Autônomo
+
+Para executar o STEP como um script autônomo, forneça os parâmetros de configuração em um arquivo JSON. Aqui está um exemplo de configuração:
+
+```json
+{
+    "secret_id": "snowflake_secret_id",
+    "source_table": "nome_da_tabela_origem",
+    "target_table": "nome_da_tabela_destino",
+    "source_on": ["coluna1", "coluna2"],
+    "target_on": ["coluna3", "coluna4"]
+}
+```
+
+Salve a configuração acima em um arquivo (por exemplo, `config.json`) e execute o script da seguinte forma:
+
+```
+python merge_tables_from_snowflake.py config.json
+```
+
+Substitua `config.json` pelo caminho do seu arquivo de configuração.
+
+## Saída
+
+Esse step `merge_tables_from_snowflake` deve ser utilizado como step final de uma pipeline, assim sendo, a variável **source_table** pode receber o mesmo nome da variável de saída do step anterior, se este step produzir um Pandas DataFrame, ou objeto que pode ser transformado em DataFrame.
--- steps/merge_tables_from_snowflake/merge_tables_from_snowflake.py
+++ steps/merge_tables_from_snowflake/merge_tables_from_snowflake.py
@@ -1,81 +1,172 @@
 from dadosfera.services.snowflake import get_snowpark_session
-from snowflake.snowpark import functions as F
-from typing import List, Dict
+from snowflake import snowpark
+import pandas as pd
+from typing import List, Dict, Union
 import logging
 import json
 import sys
 import os
 
+# Set up logging
 logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
+
+# Set logger level to DEBUG
 logger.setLevel(logging.DEBUG)
 
+# Get ORCHEST_STEP_UUID from environment variable
 ORCHEST_STEP_UUID = os.environ.get('ORCHEST_STEP_UUID')
+
 
 def merge_tables_from_snowflake(
         secret_id: str,
-        snowflake_queries_left: Dict,
-        snowflake_queries_right: Dict,
-        columns_to_merge: List[str]
-    ) -> List[str]:
-    import pandas as pd
+        source_table: Union[pd.DataFrame, str, List[Dict[str, Union[str, bytes]]]],
+        target_table: str,
+        source_on: List[str],
+        target_on: List[str]
+    ) -> None:
+    """Merge tables from Snowflake using provided queries and columns."""
+    
+    # Get Snowpark session
     session = get_snowpark_session(secret_id)
-    target = session.sql(snowflake_queries_left['queries'][0]).to_pandas()
-    source = session.sql(snowflake_queries_right['queries'][0]).to_pandas()
-    df = target.merge(source, left_on=columns_to_merge[0], right_on=columns_to_merge[1]) 
-    return df
+    session.use_schema(target_table.split(".")[0])
+    
+    # Execute Snowflake query to load target table
+    target = session.table(target_table)
 
-def orchest_handler():
-    import orchest
-    secret_id = orchest.get_step_param('secret_id')
-    left_table = orchest.get_step_param('left_table')
-    right_table = orchest.get_step_param('right_table')
-    columns_to_merge = orchest.get_step_param('columns_to_merge')
-    output_variable_name = orchest.get_step_param('output_variable_name')
+    # Check the type of source_table and convert to snowpark DataFrame if necessary
+    if isinstance(source_table, pd.DataFrame):
+        schema = get_target_schema(target)
+        source = session.create_dataframe(source_table, schema)
+    elif isinstance(source_table, list):
+        schema = get_target_schema(target)
+        source = session.create_dataframe(source_table, schema)
+    elif isinstance(source_table, str):
+        source = session.table(source_table)
+    else:
+        raise ValueError("source_table must be table from snowflake, a pandas DataFrame or a list of dictionaries.")
     
-    snowflake_queries_left = orchest.get_inputs()[left_table]
-    snowflake_queries_right = orchest.get_inputs()[right_table]
-    
-    queries = merge_tables_from_snowflake(
-        secret_id=secret_id,
-        snowflake_queries_left=snowflake_queries_left,
-        snowflake_queries_right=snowflake_queries_right,
-        columns_to_merge=columns_to_merge
+    # Construct merge condition
+    condition_clause = snowpark.functions.lit(True)
+    for i, (source_col, target_col) in enumerate(zip(source_on, target_on)):
+        condition_clause &= (target[target_col] == source[source_col])
+
+    # Prepare update and insert clauses
+    update_set_clause = {col: source[col] for col in source.columns if col in target.columns}
+    insert_set_clause = {col: source[col] for col in source.columns}
+
+    # Merge tables
+    target.merge(
+        source=source,
+        join_expr=condition_clause,
+        clauses=[
+            snowpark.functions.when_matched().update(update_set_clause),
+            snowpark.functions.when_not_matched().insert(insert_set_clause)
+        ],
+        statement_params={"ERROR_ON_NONDETERMINISTIC_MERGE": False}
     )
+
     
-    logger.info(f'Adding the following output to orchest: {queries}')
-    orchest.output(data=queries, name=output_variable_name)
+def orchest_handler():
+    """Orchest handler for running as part of an orchestration."""
+    
+    import orchest
+    
+    # Get parameters from orchest
+    secret_id = orchest.get_step_param('secret_id')
+    source_table_name = orchest.get_step_param('source_table')
+    target_table = orchest.get_step_param('target_table')
+    source_on = orchest.get_step_param('source_on')
+    target_on = orchest.get_step_param('target_on')
+    try:
+        source_table = orchest.get_inputs()[source_table_name]
+    except KeyError:
+        source_table = source_table_name
+    
+    # Merge tables
+    merge_tables_from_snowflake(
+        secret_id=secret_id,
+        source_table=source_table,
+        target_table=target_table,
+        source_on=source_on,
+        target_on=target_on
+    )
+
 
 def script_handler():
+    """Script handler for running from command line with JSON configuration."""
+    
     if len(sys.argv) != 2:
         raise Exception("Please provide the required configuration in JSON format")
+    
+    # Load configuration from JSON
     config_json = sys.argv[1]
     config = json.loads(config_json)
 
+    # Extract configuration parameters
     secret_id = config.get('secret_id')
-    input_filepath_left = config.get('input_filepath_left')
-    input_filepath_right = config.get('input_filepath_right')
-    output_filepath = config.get('output_filepath')
-    columns_to_merge = config.get('columns_to_merge')
+    input_filepath_source = config.get('input_filepath_source')
+    source_on = config.get('source_on')
+    target_on = config.get('target_on')
     
-    with open(input_filepath_left) as f:
-        snowflake_queries_left = json.loads(f.read())
-
-    with open(input_filepath_right) as f:
-        snowflake_queries_right = json.loads(f.read())
+    # Load source data from file
+    if input_filepath_source.endswith('.csv'):
+        source_table = pd.read_csv(input_filepath_source)
+    elif input_filepath_source.endswith('.json'):
+        source_table = pd.read_json(input_filepath_source)
+    elif input_filepath_source.endswith('.parquet'):
+        source_table = pd.read_parquet(input_filepath_source, engine='fastparquet')
+    else:
+        raise ValueError("input_filepath_source must be a path to a .csv, .json or .parquet file.")
         
-    queries = merge_tables_from_snowflake(
+    # Merge tables
+    merge_tables_from_snowflake(
         secret_id=secret_id,
-        snowflake_queries_left=snowflake_queries_left,
-        snowflake_queries_right=snowflake_queries_right,
-        columns_to_merge=columns_to_merge
+        source_table=source_table,
+        target_table=config.get('target_table'),
+        source_on=source_on,
+        target_on=target_on
     )
-    with open(output_filepath,'w') as f:
-        f.write(json.dumps(queries))
+
+    
+def get_target_schema(target):
+    """Script to get the schema of the target table to handle datatypes transformation."""
+    
+    # Initialize an empty list to hold the schema fields
+    schema_fields = []
+
+    # Iterate over the target schema fields
+    for tgt_field in target.schema.fields:
+        # Get the field name and datatype
+        field_name = tgt_field.column_identifier.name
+        field_datatype = str(tgt_field.datatype).split('(')[0].strip()
+
+        # Map the datatype string to the corresponding Snowpark datatype
+        if field_datatype == 'StringType':
+            datatype = snowpark.types.StringType()
+        elif field_datatype == 'IntegerType':
+            datatype = snowpark.types.IntegerType()
+        elif field_datatype == 'ArrayType':
+            datatype = snowpark.types.ArrayType()
+        elif field_datatype == 'VariantType':
+            datatype = snowpark.types.VariantType()
+        elif field_datatype == 'TimestampType':
+            datatype = snowpark.types.TimestampType()
+        elif field_datatype == 'DateType':
+            datatype = snowpark.types.DateType()
+        # Create a StructField with the field name and datatype
+        struct_field = snowpark.types.StructField(field_name, datatype)
+
+        # Add the StructField to the schema fields list
+        schema_fields.append(struct_field)
+
+    # Create a StructType with the schema fields and return schema
+    return snowpark.types.StructType(schema_fields)
+
 
 if __name__ == "__main__":
 
+    # Determine whether running as Orchest step or script
     if ORCHEST_STEP_UUID is not None:
         logger.info("Running as Orchest Step")
         orchest_handler()
