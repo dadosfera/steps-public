461555ee19873500baa570eeaa04819c456df591
Cassiana Gabriela Lima Barreto <lima.cglima@gmail.com>
2025-01-02 15:34:50 -0300

    REFACTOR: improve code structure to facilitate maintenance

--- steps/get_data_from_c2s/get_data_from_c2s.py
+++ steps/get_data_from_c2s/get_data_from_c2s.py
@@ -10,216 +10,241 @@ import orchest
 from dadosfera.services.snowflake import get_snowpark_session
 
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-
-BASE_URL = orchest.get_step_param('url')
-TOKEN = os.getenv('C2S_AUTHENTICATOR_TOKEN')
-
+ORCHEST_STEP_UUID = os.environ.get('ORCHEST_STEP_UUID')
 
 LAST_UPDATE_PATH = "last_update.json"
 PER_PAGE = 50
 MAX_REQ_PER_MINUTE = 10
 
 
-def get_last_update():
+class C2SLead:
     """
-    Lê o 'last_update' de um arquivo JSON (LAST_UPDATE_PATH).
-    Retorna None se não existir ou se houver falha.
-    Exemplo: {"last_update": "2024-12-27T10:46:28Z"}
+    Class to fetch leads from C2S API.
+
+    Attributes:
+    - logger: logging.Logger
+    - instance_url: str
+    - token: str
     """
-    if not os.path.exists(LAST_UPDATE_PATH):
-        logger.info(f"[get_last_update] Arquivo {
-                    LAST_UPDATE_PATH} não encontrado. Retornando None.")
-        return None
 
-    try:
-        with open(LAST_UPDATE_PATH, "r") as f:
-            data = json.load(f)
-            last_update = data.get("last_update")
-            logger.info(
-                f"[get_last_update] last_update carregado: {last_update}")
-            return last_update
-    except Exception as e:
-        logger.error(f"[get_last_update] Erro ao carregar o arquivo {
-                     LAST_UPDATE_PATH}: {e}")
-        return None
+    def __init__(
+        self,
+        logger: logging.Logger,
+        instance_url: str,
+        token: str
+    ):
+        self.logger = logger
+        self.instance_url = instance_url
+        self.token = token
 
+    def get_last_update(self):
+        """
+        Reads the 'last_update' from a JSON file (LAST_UPDATE_PATH).
+        Returns None if it doesn't exist or if there's a failure.
+        Example: {"last_update": "2024-12-27T10:46:28Z"}
+        """
+        if not os.path.exists(LAST_UPDATE_PATH):
+            self.logger.info(f"[get_last_update] File {
+                             LAST_UPDATE_PATH} not found. Returning None.")
+            return None
 
-def save_last_update(timestamp_str):
-    """
-    Salva o timestamp (ex.: '2024-01-01T00:00:00Z') em arquivo JSON.
-    """
-    try:
-        with open(LAST_UPDATE_PATH, "w") as f:
-            json.dump({"last_update": timestamp_str}, f)
-        logger.info(f"[save_last_update] Valor '{
-                    timestamp_str}' salvo com sucesso em {LAST_UPDATE_PATH}.")
-    except Exception as e:
-        logger.error(f"[save_last_update] Erro ao salvar no arquivo {
-                     LAST_UPDATE_PATH}: {e}")
-        raise
-
-
-def fetch_data(last_update=None):
-    """
-    Faz a chamada à API, usando 'updated_gte' como filtro se existir.
-    Retorna a lista de leads.
-    Respeita a limitação de 10 requisições por minuto.
-    """
-    headers = {
-        'Authorization': f'Bearer {TOKEN}',
-        'Content-Type': 'application/json'
-
-    }
-    all_results = []
-    page = 1
-    count = 0
-
-    while True:
-        params = {
-            "page": page,
-            "perpage": PER_PAGE
-        }
-        # Só adiciona last_update se existir (ou seja, se não for a primeira execução).
-        if last_update:
-            params["updated_gte"] = last_update
-
-        logger.info(f"[fetch_data] Requisição página {
-                    page}, updated_gte={last_update}")
         try:
-            response = requests.get(BASE_URL, headers=headers, params=params)
-            response.raise_for_status()
-            data = response.json()
-
-            leads = data.get("data", [])
-            if leads:
-                all_results.extend(leads)
-            total = data.get("meta", {}).get("total", 0)
-
-            logger.info(f"[fetch_data] Página {page} retornou {
-                        len(leads)} leads. total={total}")
-
-            # Se a paginação estiver concluída, saímos do loop
-            if (PER_PAGE * page) >= total:
-                logger.info(
-                    "[fetch_data] Finalizando a coleta (todas as páginas)...")
-                break
-
-            page += 1
-            count += 1
-
-            # api wait 10 req per minute
-            if count == MAX_REQ_PER_MINUTE:
-                logger.info(
-                    "[fetch_data] Limite de 10 req/min atingido: aguardando 60 segundos...")
-                time.sleep(60)
-                count = 0
-
-        except requests.exceptions.HTTPError as e:
-            logger.error(f"[fetch_data] Request falhou na página {page}: {e}")
-            raise e
+            with open(LAST_UPDATE_PATH, "r") as f:
+                data = json.load(f)
+                last_update = data.get("last_update")
+                self.logger.info(
+                    f"[get_last_update] Loaded last_update {last_update}")
+                return last_update
         except Exception as e:
-            logger.error(f"[fetch_data] Erro inesperado na página {page}: {e}")
-            raise e
+            self.logger.error(f"[get_last_update] Error loading file{
+                              LAST_UPDATE_PATH}: {e}")
+            return None
 
-    return all_results
+    def save_last_update(self, timestamp_str):
+        """
+        Saves the 'last_update' to a JSON file (LAST_UPDATE_PATH).
+        """
+        try:
+            with open(LAST_UPDATE_PATH, "w") as f:
+                json.dump({"last_update": timestamp_str}, f)
+            self.logger.info(f"[save_last_update] Saved '{timestamp_str}' to {
+                             LAST_UPDATE_PATH} successfully.")
+        except Exception as e:
+            self.logger.error(f"[save_last_update] Error saving file {
+                              LAST_UPDATE_PATH}: {e}")
+            raise
 
+    def fetch_data(self, last_update=None):
+        """
+        Fetches all leads from the C2S API.
+        last_update: str - ISO 8601 format (ex.: "2024-12-27T10:46:28Z")
+        Returns a list of leads (dicts).
+        """
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json'
 
-def find_max_updated_at(leads):
-    """
-    Percorre a lista de leads e identifica o maior valor de 'updated_at'.
-    Retorna o timestamp no formato 'YYYY-MM-DDTHH:MM:SSZ'.
-    """
-    max_dt = None
+        }
+        all_results = []
+        page = 1
+        count = 0
 
-    for lead in leads:
-        attributes = lead.get("attributes", {})
-        lu = attributes.get("updated_at")  # ex.: "2024-12-27T10:46:28Z"
-        if lu:
+        while True:
+            params = {
+                "page": page,
+                "perpage": PER_PAGE
+            }
+
+            if last_update:
+                params["updated_gte"] = last_update
+
+            self.logger.info(f"[fetch_data] Requesting page {
+                             page}, updated_gte={last_update}")
             try:
-                dt = datetime.datetime.fromisoformat(lu.replace("Z", "+00:00"))
-                dt_utc = dt.astimezone(datetime.timezone.utc)
-                if not max_dt or dt > max_dt:
-                    max_dt = dt_utc
-            except ValueError:
-                # Caso o formato não seja compatível, ignora e segue
-                logger.warning(f"[find_max_updated_at] Formato inválido: {lu}")
-                pass
+                response = requests.get(
+                    self.instance_url, headers=headers, params=params)
+                response.raise_for_status()
+                data = response.json()
 
-    if max_dt:
-        formatted = max_dt.strftime("%Y-%m-%dT%H:%M:%SZ")
-        return formatted
-    return None
+                leads = data.get("data", [])
+                if leads:
+                    all_results.extend(leads)
+                total = data.get("meta", {}).get("total", 0)
 
+                self.logger.info(f"[fetch_data] Page {page} returned {
+                                 len(leads)} leads. Total={total}")
 
-def save_to_snowflake(snowpark, all_results):
-    """
-    Recebe a lista de leads (all_results) e salva no Snowflake via Snowpark.
-    """
-    try:
-        logger.info(
-            "[save_to_snowflake] Convertendo lista em DataFrame Snowpark...")
-        df_leads = snowpark.createDataFrame(all_results)
+                if (PER_PAGE * page) >= total:
+                    self.logger.info(
+                        "[fetch_data] Completed fetching all pages.")
+                    break
 
-        table_identifier = "DADOSFERA_PRD_IBRAM.PROLAR.TB_RAW_LEADS"
-        logger.info(f"[save_to_snowflake] Salvando DataFrame na tabela {
-                    table_identifier} (modo append)...")
-        df_leads.write.mode("append").save_as_table(table_identifier)
-        logger.info(f"[save_to_snowflake] DataFrame salvo com sucesso em {
-                    table_identifier}.")
-    except Exception as e:
-        logger.error(
-            f"[save_to_snowflake] Erro ao salvar dados no Snowflake: {e}")
-        raise e
+                page += 1
+                count += 1
 
+                # api wait 10 req per minute
+                if count == MAX_REQ_PER_MINUTE:
+                    self.logger.info(
+                        "[fetch_data] Rate limit reached: waiting 60 seconds...")
+                    time.sleep(60)
+                    count = 0
 
-def main():
-    """
-    Fluxo principal
-    """
-    secret_id = os.getenv("SECRET_ID")
-    snowpark = get_snowpark_session(secret_id)
-    snowpark.use_warehouse('COMPUTE_WH')
-    snowpark.use_schema('PROLAR')
+            except requests.exceptions.HTTPError as e:
+                self.logger.error(
+                    f"[fetch_data] Request failed on page {page}: {e}")
+                raise e
+            except Exception as e:
+                logger.error(
+                    f"[fetch_data] Unexpected error on page {page}: {e}")
+                raise e
 
-    try:
-        last_update = get_last_update()
-        if last_update:
-            logger.info(
-                f"[main] INCREMENTAL a partir de updated_at={last_update}")
-        else:
-            logger.info("[main] FULL LOAD - Nenhum 'last_update' encontrado")
+        return all_results
 
-        leads = fetch_data(last_update)
-        logger.info(f"[main] Total de registros obtidos: {len(leads)}")
+    def find_max_updated_at(self, leads):
+        """
+        Finds the maximum 'updated_at' timestamp from a list of leads.
+        Returns a string in ISO 8601 format (ex.: "2024-12-27T10:46:28Z").
+        """
+        max_dt = None
 
-        if leads:
-            save_to_snowflake(snowpark, leads)
-            logger.info("[main] Salvando leads no Snowflake...")
+        for lead in leads:
+            attributes = lead.get("attributes", {})
+            lu = attributes.get("updated_at")  # ex.: "2024-12-27T10:46:28Z"
+            if lu:
+                try:
+                    dt = datetime.datetime.fromisoformat(
+                        lu.replace("Z", "+00:00"))
+                    dt_utc = dt.astimezone(datetime.timezone.utc)
+                    if not max_dt or dt > max_dt:
+                        max_dt = dt_utc
+                except ValueError:
+                    # Caso o formato não seja compatível, ignora e segue
+                    self.logger.warning(
+                        f"[find_max_updated_at] Invalid format:  {lu}")
+                    pass
 
-            new_last_update = find_max_updated_at(leads)
-            logger.info(f"[main] Valor encontrado: {new_last_update}")
+        return max_dt.strftime("%Y-%m-%dT%H:%M:%SZ") if max_dt else None
 
-            if new_last_update:
-                save_last_update(new_last_update)
-                logger.info(f"[main] Arquivo '{LAST_UPDATE_PATH}' atualizado.")
+    def run(self):
+        """
+        Main method to orchestrate the process.
+        """
+
+        try:
+            last_update = self.get_last_update()
+            if last_update:
+                self.logger.info(
+                    f"[run] Incremental load from updated_at={last_update}")
             else:
-                logger.info(
-                    "[main] Nenhum 'updated_at' válido encontrado; nada a salvar.")
+                self.logger.info("[run] Full load - No 'last_update' found.")
 
-        else:
-            logger.info("[main] Nenhum lead retornado.")
+            leads = self.fetch_data(last_update)
+            logger.info(f"[main] Total leads fetched: {len(leads)}")
 
-    except Exception as e:
-        logger.info(f"Ocorreu um erro: {e}")
-        raise
-    finally:
-        snowpark.close()
+            if leads:
+                new_last_update = self.find_max_updated_at(leads)
+                if new_last_update:
+                    self.save_last_update(new_last_update)
+                else:
+                    self.logger.info(
+                        "[run] No valid 'updated_at' found to save.")
+
+        except Exception as e:
+            self.logger.error(f"Error occured: {e}")
+            raise
+
+
+def orchest_handler():
+    base_url = orchest.get_step_param('url')
+    token = os.getenv("C2S_AUTHENTICATOR_TOKEN")
+
+    if not token:
+        raise Exception("C2S_AUTHENTICATOR_TOKEN is required")
+
+    handler = C2SLead(
+        logger=logger,
+        instance_url=base_url,
+        token=token
+    )
+    handler.run()
+
+
+def script_handler():
+    if len(sys.argv) != 2:
+        raise Exception(
+            "Please provide the required configuration in JSON format")
+    config_json = sys.argv[1]
+    config = json.loads(config_json)
+
+    base_url = config.get("url")
+    token = config.get("token")
+
+    if not base_url or not token:
+        raise ValueError(
+            "Both 'url' and 'token' must be provided in the configuration.")
+
+    handler = C2SLead(
+        logger=logger,
+        instance_url=base_url,
+        token=token
+    )
+    handler.run()
 
 
 if __name__ == "__main__":
 
-    main()
+    if ORCHEST_STEP_UUID is not None:
+        logger.info("Running as Orchest Step")
+        orchest_handler()
+    else:
+        logger.info("Running as script")
+        script_handler()
